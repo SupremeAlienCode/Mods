3ec21c9c1fe2ada0f3c1363d16cbf26e1c3da698
SupremeAlienCode <supremealiencode.contato@gmail.com>
2025-11-12 13:43:34 -0300

    Add files via upload

---
+++ mods.js
@@ -0,0 +1,17 @@
+// mods.js
+const mods = [
+  {
+    id: "chocolate-dreams",
+    name: "Chocolate Dreams",
+    version: "1.21.0",
+    description: "Blocos de chocolates e muito mais!",
+    downloadUrl: "https://edge.forgecdn.net/files/7214/479/Chocolate%20addon%20v2-1.mcaddon?ft=47991a31ced8af7326da6198e2b8dda9&bd=9rsn7l32ty"
+  },
+  {
+    id: "otherside",
+    name: "Otherside - Dimensão da Caveira",
+    version: "1.21.0",
+    description: "Adiciona uma nova dimensão de caveira.",
+    downloadUrl: "https://api.mcpedl.com/api/download/467553/?r=..."
+  }
+];
